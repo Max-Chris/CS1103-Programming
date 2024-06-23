e2ecfc8f548d5fd05f2a29458b43b51c7f4d6ade
Maxwell Ofoegbu <livingepistlesnet@yahoo.com>
2024-06-23 14:29:55 +0100

    commit for person object

---
+++ Person/src/com/in28minutes/oops/level2/inheritance/Employee.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.in28minutes.oops.level2.inheritance;
+
+/**
+ * 
+ */
+public class Employee {
+
+	/**
+	 * 
+	 */
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
