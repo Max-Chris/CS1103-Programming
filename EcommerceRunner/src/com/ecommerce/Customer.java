1bbaca6f5fb23547223fc32fe6fc65206579ad36
Maxwell Ofoegbu <livingepistlesnet@yahoo.com>
2024-07-03 05:39:28 -1000

    EcommerceRunner/

---
+++ EcommerceRunner/src/com/ecommerce/Customer.java
@@ -0,0 +1,15 @@
+package com.ecommerce;
+
+public class Customer {
+	
+	private int customerID;
+	private String name;
+	private String shoppingCart;
+
+		//Implement methods to add and remove products from the shopping cart, 
+		//calculate the total cost, and place orders.  
+	public int add(int ty) {
+		
+		return ty;
+	}
+}
